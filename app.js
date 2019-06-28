0985623853ca8bee7edfe64733d7e81de965ea7a
nmtoan251998 <nmtoan251998@gmail.com>
2019-06-28 16:32:07 +0700

    FrontEnd-BackEnd configuration

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const Router = require('./routes/index.routes.js');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(logger('dev'));
 app.use(express.json());
