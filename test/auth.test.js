48be58b129d62639a03905c5e2e44988cb09a086
nmtoan251998 <nmtoan251998@gmail.com>
2019-07-03 20:21:38 +0700

    documentation: documenting...

--- test/auth.test.js
+++ test/auth.test.js
@@ -4,42 +4,145 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app');
-const should = chai.should();
 const expect = chai.expect;
+
+const User = require('../model/users.model');
 
 chai.use(chaiHttp);
 
-describe('App basics', () => {
-    it('Server should exists', () => {
-        expect(server).to.be.a('function');
-    })
-});
+describe('User Authentication', () => {        
 
-describe('User Authentication', () => {    
-    describe('Login', () => {
-        it('It should return 404 if wrong account or password' ,(done) => {
-            // Mock valid user
-            const validUser = {
+    describe('Registration', () => {         
+        it('Should return 409 when account already exist', (done) => {
+            // existAccount
+            const newUser = {
+                username: 'Test',
                 accountname: 'test',
-                password: '12356'
+                password: '123456',
+                password2: '123456',
+            }
+
+            chai.request(server)
+                .post('/auth/register')
+                .send(newUser)
+                .then(res => {                                        
+                    expect(res).to.have.status(409);                    
+                    expect(res.body.error).to.be.a('object');
+                    expect(res.body.error.length).to.not.equal(0);                    
+                    expect(res.body.success).to.be.equal(false);
+                    expect(res.body.error.accountExist).to.be.equal('This account name already exist');                    
+                    done();
+                })
+                .catch(err => {
+                    done(err);               
+                })
+        });                
+
+        it('Should return 400 when validations failed', (done) => {
+            // mock invalid user
+            const invalidUser = {
+                username: '',
+                accountname: 'testtest',
+                password: '123456',
+                password2: '123456',
+            }
+
+            chai.request(server)
+                .post('/auth/register')
+                .send(invalidUser)
+                .then(res => {                                        
+                    expect(res).to.have.status(400);
+                    expect(res.body.error).to.be.a('object');
+                    expect(res.body.error.length).to.not.equal(0);                    
+                    expect(res.body.success).to.be.equal(false);                    
+                    done();
+                })
+                .catch(err => {
+                    done(err);               
+                })
+        });                
+
+        it('Should return 200 when a new user is created', (done) => {
+            // mock valid user
+            const newUser = {
+                username: 'Test',
+                accountname: 'test',
+                password: '123456',
+                password2: '123456',
+            }
+
+            // before creating user, check for its existence
+            User.findOne({ accountname: 'test' })
+            .then(user => {                    
+                if(user) {                        
+                    User.findByIdAndRemove(user.id)
+                        .then((user) => {                            
+                            chai.request(server)
+                                .post('/auth/register')
+                                .send(newUser)
+                                .then(res => {                    
+                                    expect(res).to.have.status(200);                                                       
+                                    expect(res.body.success).to.be.equal(true);
+                                    expect(res.body.newUser).to.be.a('object');
+                                    done();
+                                })
+                                .catch(err => {
+                                    done(err);               
+                                })
+                        })
+                        .catch((err) => console.log('Error removing for testing'));
+                }
+            })                        
+        });                
+    })
+
+    describe('Login', () => {
+        it('It should return 404 if account or password is wrong', (done) => {
+            // Mock invalid user
+            const invalidUser = {
+                accountname: 'invalidUser',
+                password: '123456'
+            }            
+
+            chai.request(server)
+                .post('/auth/login')
+                .send(invalidUser)
+                .then((res) => {                             
+                    expect(res).to.have.status(404);
+                    expect(res.body.error).to.be.a('object');
+                    expect(res.body.error.length).to.not.equal(0);
+                    expect(res.body.success).to.be.equal(false);
+                    expect(res.body.error.wrongAccount).to.be.equal('Wrong accountname or password');
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                })
+        });
+
+        it('Should return 400 when validation failed', (done) => {
+            // mock unvalid user
+            const invalidUser = {                
+                accountname: '',
+                password: '123456',                
             }
 
             chai.request(server)
                 .post('/auth/login')
-                .send(validUser)
-                .then((res) => {                             
-                    expect(res).to.have.status(404);
-                    expect(res.body).to.be.a('object');   
-                    expect(res.body.error).to.be.a('object');   
-                    expect(res.body.error.userNotFound).to.be.equal('Wrong accountnamne or password');                                     
+                .send(invalidUser)
+                .then(res => {                    
+                    expect(res).to.have.status(400);
+                    expect(res.body.error).to.be.a('object');
+                    expect(res.body.error.length).to.not.equal(0);                    
+                    expect(res.body.success).to.be.equal(false);                    
                     done();
                 })
                 .catch(err => {
-                    done();
+                    done();               
                 })
         });
 
-        it('It should return 200 with login information', (done) => {
+        it('It should return 200 when user successfully log in', (done) => {
             // Mock valid user
             const validUser = {
                 accountname: 'test',
@@ -51,71 +154,17 @@ describe('User Authentication', () => {
                 .send(validUser)
                 .then((res) => {                    
                     expect(res).to.have.status(200);
+                    expect(res.body.success).to.be.equal(true);
                     expect(res.body.token).to.exist;
-                    expect(res.body).to.be.a('object');                    
+                    expect(res.body.userId).to.exist;
+                    expect(res.body.username).to.exist;
+                    expect(res.body.accountname).to.be.equal(validUser.accountname);                    
+                    expect(res.body).to.be.a('object');                                        
                     done();
                 })
                 .catch(err => {
                     done(err);                    
                 })
         });
-    })
-
-    describe('Registration', () => { 
-        it('Should return 409 or 400 and error object if errors exist', (done) => {
-            // mock valid user
-            const newUser = {
-                username: 'TestTest',
-                accountname: 'testtest',
-                password: '123456',
-                password2: '123456',
-            }
-
-            chai.request(server)
-                .post('/auth/register')
-                .send(newUser)
-                .then(res => {                    
-                    if(res.status === 400) {
-                        expect(res).to.have.status(400);
-                        expect(res.body).to.be.a('object');
-                        expect(res.body.error).to.be.a('object');
-                        expect(res.body.error.failToRegister).to.be.equal('Fail to register new user');
-                        return done();
-                    }        
-                    
-                    expect(res).to.have.status(409);
-                    expect(res.body).to.be.a('object');
-                    expect(res.body.error).to.be.a('object');
-                    expect(res.body.error.accountAxist).to.be.equal('This account name already axist');                    
-                    done();
-                })
-                .catch(err => {
-                    done();               
-                })
-        });                
-
-        it('Should return 200 and register information', (done) => {
-            // mock valid user
-            const newUser = {
-                username: 'TestTestTest',
-                accountname: 'testtesttest',
-                password: '123456',
-                password2: '123456',
-            }
-
-            chai.request(server)
-                .post('/auth/register')
-                .send(newUser)
-                .then(res => {                    
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.be.a('object');                                        
-                    expect(res.body.success).to.be.equal(true);
-                    expect(res.body.msg).to.be.equal(`Create user ${newUser.username}`);
-                    done();
-                })
-                .catch(err => {
-                    done();               
-                })
-        });                
     })
 })
--- test/users.test.js
+++ test/users.test.js
@@ -4,62 +4,81 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app');
-const should = chai.should();
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-let token;
-
-describe('Protected routes', () => {
-    beforeEach((done) => {
-        chai.request(server)
-            .post('/auth/login')
-            .send({
-                accountname: 'nmtoan',
-                password: '123456'
-            })
-            .end((err, res) => {
-                token = res.body.token;                
-                done();
-            })
-    });    
-
-    describe('GET /api/users/all', () => {
-        it('it should get all current users', (done) => {
+describe('Public routes', () => {
+    describe('Get all users', () => {
+        it('Should return 200 when users array is returned', (done) => {
             chai.request(server)
                 .get('/api/users/all')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('array');
-
+                .then((res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('object');
+                    expect(res.body.users).to.be.a('array');                    
+                    expect(res.body.success).to.be.equal(true);
                     done();
                 })
-        })
-    });
+                .catch(err => done(err));
+        });        
 
-    describe('GET /api/users', () => {
-        // token not being sent - should respond with a 401
-        it('It should require authorization', (done) => {
+        it('Should return 404 when no users found', (done) => {
             chai.request(server)
-                    .get('/api/users')
-                    .end((err, res) => {
-                        res.statusCode.should.eql(401);
-                        done();
-                    })
-        })
-    })    
-
-        // it('It should respond with JSON when token is set', (done) => {
-        //     chai.request(server)                    
-        //             .get('/api/users')    
-        //             .set('Authorization', `Bearer ${token}`)
-        //             .end((err, res) => {
-        //                 res.statusCode.should.eql(200);
-        //                 // res.type.should.eql('application/json');                        
-
-        //                 done();
-        //             })
-        // })
+                .get('/api/users/all')
+                .then((res) => {
+                    expect(res).to.have.status(404);
+                    expect(res.body).to.be.a('object');
+                    expect(res.body.error).to.be.a('object'); 
+                    expect(res.body.error.length).to.not.equal(0); 
+                    expect(res.body.error.noUsers).to.be.equal('No users found'); 
+                    done();
+                })
+                .catch(err => done(err));
+        });        
+    })
 })
+
+// describe('Private routes', () => {   
+//     let token;
+
+//     beforeEach((done) => {
+//         chai.request(server)
+//             .post('/auth/login')
+//             .send({
+//                 accountname: 'nmtoan',
+//                 password: '123456'
+//             })
+//             .end((err, res) => {
+//                 token = res.body.token;                
+//                 done();
+//             })
+//     });    
+
+//     describe('GET /api/users/all', () => {
+//         it('it should get all current users', (done) => {
+//             chai.request(server)
+//                 .get('/api/users/all')
+//                 .end((err, res) => {
+//                     res.should.have.status(200);
+//                     res.body.should.be.a('array');
+
+//                     done();
+//                 })
+//         })
+//     });
+
+//     describe('GET /api/users', () => {
+//         // token not being sent - should respond with a 401
+//         it('It should require authorization', (done) => {
+//             chai.request(server)
+//                     .get('/api/users')
+//                     .end((err, res) => {
+//                         res.statusCode.should.eql(401);
+//                         done();
+//                     })
+//         })
+//     })        
+// })
 
 
